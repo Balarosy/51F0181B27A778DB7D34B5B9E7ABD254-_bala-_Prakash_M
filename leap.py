092771dbd596462d488c9b9c18871d6c8c2d3586
Balarosy <147149723+Balarosy@users.noreply.github.com>
2023-10-10 14:01:46 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,12 @@
+def isleapyear(year):
+  if ( year )
+            return True
+else:
+   return False
+ year = 2013
+if isleapyear(year):
+  print ('{} is a leap year.'.format(year))
+else:
+   print ('{} is not a leap year.'.format(year))
+
+
